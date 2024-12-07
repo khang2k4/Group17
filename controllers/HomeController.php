991695fdaf82ced585cab43da62a7fbc4cb1b296
Hoàng Minh Sáng <144579641+hoangminhsang04@users.noreply.github.com>
2024-12-07 20:34:04 +0700

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,16 +2,18 @@
 require_once 'models/News.php';
 
 class HomeController {
+    //hiển thị
     public function index() {
         $news = News::getAll();
         include 'views/news/index.php';
     }
-
+//xem chi tiết
     public function detail() {
         $id = $_GET['id'];
         $newsItem = News::getById($id);
         include 'views/news/detail.php';
     }
+    //tìm kiếm
     public function search() {
         $keyword = $_GET['keyword'] ?? '';
         $news = News::search($keyword);
