cde0e0ef4b2a678b79b5c215105fa0329923d0b4
sonvanhleg <167208481+sonvanhleg@users.noreply.github.com>
2024-12-07 20:42:24 +0700

    User.php

--- models/User.php
+++ models/User.php
@@ -1,13 +1,13 @@
 <?php
 require_once 'config/connDB.php';
 class User{
+    //Xác thực người dùng dựa trên tên đăng nhập và mật khẩu
     public static function authenticate($username, $password){
         $db = connDB::getConnection();
         $stmt = $db->prepare("SELECT * FROM users WHERE username =? ");
         $stmt->execute([$username]);
         $user = $stmt->fetch();
 
-          // Kiểm tra nếu người dùng tồn tại và mật khẩu khớp
         // So sánh mật khẩu từ cơ sở dữ liệu với mật khẩu đầu vào
         if($user && $password === $user['password']){
             return $user;
