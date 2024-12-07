600ff025f0e3314d57a073c4e283b9465b1943c9
Hoàng Minh Sáng <144579641+hoangminhsang04@users.noreply.github.com>
2024-12-07 20:37:21 +0700

    Update NewsController.php

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -6,23 +6,26 @@ class NewsController
 
     public function __construct()
     {
-        $this->newsModel = new News();
+        $this->newsModel = new News();// Khởi tạo đối tượng model News và gán cho thuộc tính $newsModel.
     }
 
     //Hien thi danh sach tin tuc
     public function index()
     {
-        $news = $this->newsModel->getAll();
-        require 'views/news/index.php';
+        $news = $this->newsModel->getAll();//gọi getall
+        require 'views/news/index.php';//hiển thị trang giao diện
     }
 
     //Hien thi chi tiet tin tuc
     public function detail($id)
     {
+        //lấy thông tin chi tiết dựa trên id
         $newsItem = $this->newsModel->getById($id);
         if ($newsItem) {
+            //nếu tồn tại hiển thị giao diện chính
             require 'views/news/detail.php';
         } else {
+            //nếu k tồn tại hiển thị thông báo
             echo "Tin tức không tồn tại!";
         }
     }
